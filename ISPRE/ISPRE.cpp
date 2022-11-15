2123aa02a7931de35f8a54dbb563b3cd41712148
Brent George <brentdgeorge1@gmail.com>
2022-11-14 20:13:25 -0500

    more project scaffolding

---
+++ ISPRE/ISPRE.cpp
@@ -0,0 +1,40 @@
+//===----------------------------------------------------------------------===//
+//
+//  INSERT DESCRIPTION OF FILE HERE
+//
+////===----------------------------------------------------------------------===//
+#include "llvm/Analysis/BlockFrequencyInfo.h"
+#include "llvm/Analysis/BranchProbabilityInfo.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopIterator.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Scalar/LoopPassManager.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/LoopUtils.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "ispre"
+
+namespace ISPRE {
+class ISPREPass : public FunctionPass {
+    // TODO
+    bool runOnFunction(Function &F) override { return false; }
+
+  public:
+    static char ID;
+    ISPREPass() : FunctionPass(ID) {}
+
+  private:
+    static int dummyVar;
+};
+} // namespace ISPRE
+
+char ISPRE::ISPREPass::ID = 0;
+static RegisterPass<ISPRE::ISPREPass>
+    X("ispre", "Isothermal Speculative Partial Redundancy Elimination", false, false);
---
+++ README.md
@@ -0,0 +1,16 @@
+# Isothermal Speculative Partial Redundancy Elimination
+
+This project partially fulfills the requirements for the EECS 583 Compilers course at the University of Michigan.
+
+## Setup
+
+1. Clone this repository, and create a build directory.
+    ```
+    user@machine:/{this-repo}/ $ mkdir build
+    user@machine:/{this-repo}/ $ cd build
+    ```
+2. Enter the build directory and generate the Makefile.
+    ```
+    user@machine:/{this-repo}/build $ cmake ..
+    user@machine:/{this-repo}/build $ make
+    ```
