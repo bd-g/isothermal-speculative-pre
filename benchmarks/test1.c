a61ad2c7e9d6eb239cd60f0f42e87ec2800fd3bb
PriTanir <41167321+PriTanir@users.noreply.github.com>
2022-11-16 14:57:06 -0500

    Add files via upload
    These only test LLVM PRE

---
+++ benchmarks/test1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* Basic test case from Effective PRE paper that can be used to test LLVM PRE. No loops, just a branch. 
+The else branch is always taken so x+y is calculated twice when it doesnt change at all.
+Output after PRE in CFG should be : x+y removed from outside the branches and into the if branch*/
+
+int main() {
+    int a=0;
+    int b=0;
+    int x=0;
+    int y=1;
+    int condition = 6;
+    if(condition==5)
+    {
+        y=2;
+    }
+    else
+    {
+        x=2;
+        a=x+y;
+
+    }
+    b=x+y;
+    
+    printf("%d\n", b);
+
+    return 0;
+}
---
+++ benchmarks/test2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* Test case from ISPRE paper that can be used to test LLVM PRE. With loop. Must print 251.
+a+b is loop invariant if sum%10 is not 0. After PRE, CFG should be 
+t1=a+b;
+while(t1>sum)
+same if but added t1=a+b. */
+
+int main() {
+    int a=200;
+    int b=46;
+    int sum=205;
+    while(a+b>sum)
+    {
+        if(sum%10==0)
+        {
+            a=a+1;
+        }
+        sum=sum+b;
+    }
+    
+    printf("%d\n", sum);
+
+    return 0;
+}
